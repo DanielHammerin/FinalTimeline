13e45b97c5418792b9bb1eac99f1aeb2a3a4e4d5
Mauro <mauropappaterra@gmail.com>
2015-04-09 10:09:06 +0200

    lalala

--- Timeline/src/model/databaseTest.java
+++ Timeline/src/model/databaseTest.java
@@ -1,11 +1,24 @@
 package model;
 
+import java.util.Date;
+
+import com.db4o.*;
+import com.tedneward.model.*;
+
 public class databaseTest {
 
 	public static void main(String[] args) {
-
-		Event
-
+		
+		Date start = new Date (2014-9-1);
+		Date finish = new Date (2015-1-1);
+		
+		EventTime firstSemester = new EventTime ("First Semester", "First Semerster for Software Technology Program", start, finish);
+		
+		Date start2 = new Date (2015-3-15);
+		Date finish2 = new Date (2015-6-7);
+		
+		EventTime secondSemester = new EventTime ("Second Semester", "Second Semerster for Software Technology Program", start2, finish2);
+		
+		
+		}
 	}
-
-}
