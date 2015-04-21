e49fb221c5868d81adcda36c06c0dceb76672fb5
Hatem <hatem.houssein@gmail.com>
2015-04-21 10:22:24 +0200

    Database access object implementation and interface (we are not gonna use it now).

--- Timeline/src/controller/DAO.java
+++ Timeline/src/controller/DAO.java
@@ -1,60 +1,96 @@
 package controller;
 /** This class is the implementation of the database access object implementation **/
-import java.util.List;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import com.db4o.Db4o;
 import com.db4o.ObjectContainer;
 import com.db4o.ObjectSet;
-
 import model.MyEvent;
 import model.Timeline;
 
 public class DAO implements DaoInterface { // Data Access Objects
-	private List<Timeline> timelines;
+
+	private ArrayList<Timeline> timelines;
+
 	
 	@Override
 	public void addData (ObjectContainer database, Timeline newTimeline) {
 
-		boolean flag = true;
-		
-//		ObjectSet<Timeline> retrieve = database.query (Timeline.class); // created object to retrieve info from the database
+		//		boolean flag = true;
+		//
+		//		//		ObjectSet<Timeline> retrieve = database.query (Timeline.class); // created object to retrieve info from the database
+		//
+		//		//		while (retrieve.hasNext()){// THERE IS AN ERROR IN THIS LOOP
+		//		//			if (addTimeline.equals(retrieve.next())){
+		//		//				flag = false;
+		//		//			}
+		//		//		}
+		//
+		//		if (flag){
+		//			database.store(newTimeline);
+		//			database.commit();
+		//		} else {
+		//			System.out.println( "The Timeline has been already added in the database!");
+		//		}	
 
-//		while (retrieve.hasNext()){// THERE IS AN ERROR IN THIS LOOP
-//			if (addTimeline.equals(retrieve.next())){
-//				flag = false;
-//			}
-//		}
-		
-		if (flag){
-			database.store(newTimeline);
-			database.commit();
-		} else {
-			System.out.println( "The Timeline has been already added in the database!");
+		//Hatem's implementation
+		ObjectContainer db = null;
+		try {
+			db = Db4o.openFile("timelineDatabase.data");
+			db.store(newTimeline);
+			db.commit();
 		}
-		
+		finally{
+			if(db != null) {
+				db.close();
+			}
+		}
 	}
+
 	@Override
-	public List<Timeline> getData() {
-		if(timelines.isEmpty()) {
-			throw new IllegalArgumentException("There is no data to retrieve");
-		}
-		return timelines;
+	public ArrayList<ObjectSet<Timeline>> getTimeline(String title) {
+
+		ObjectContainer data = Db4o.openFile("timelineDatabase.data");
+		ObjectSet<Timeline> retrievedData = data.get(new Timeline(title, "") {});
+
+		ArrayList<ObjectSet<Timeline>> sameTimelines = new ArrayList<>();
+		sameTimelines.add(retrievedData);
+		return sameTimelines;
 	}
+
 	@Override
 	public void deleteAll() {
-		// TODO Auto-generated method stub
-		
+		timelines.clear();
+		System.out.println("All data has been deleted.");
 	}
-	
+
 	@Override
 	public void deleteTimeline(Timeline data) {
 		if(timelines.remove(data)) {System.out.println("Timeline is removed."); }
 		else { throw new IllegalArgumentException ("Timeline is not removed.");}
 	}
-	
+
 	@Override
 	public void deleteEvent(MyEvent data) {
 		// TODO Auto-generated method stub
-		
+
 	}
-	
+
+	private Iterator<Timeline> iterator() {
+		return new TimelineIterator();
+	}
+
+	private class TimelineIterator implements Iterator<Timeline> {
+		private int position = 0;
+
+		public boolean hasNext() {
+			return position < timelines.size();
+		}
+
+		public Timeline next() {
+			return timelines.get(position++);
+		}
+	}
+
 }
--- Timeline/src/controller/DaoInterface.java
+++ Timeline/src/controller/DaoInterface.java
@@ -3,15 +3,18 @@ package controller;
  * This is an interface for the database access object implementation.
  */
 
-import java.util.List;
+import java.util.ArrayList;
+
 import com.db4o.ObjectContainer;
+import com.db4o.ObjectSet;
+
 import model.MyEvent;
 import model.Timeline;
 
 public interface DaoInterface {
 
 	public void addData (ObjectContainer database ,Timeline addTimeline);	//Saves all the data in the database.
-	public List<Timeline> getData();	//Retrieves all the data from the database.
+	public ArrayList<ObjectSet<Timeline>> getTimeline(String title);	//Retrieves all the data from the database.
 	public void deleteAll();	//Deletes all the data in the database.
 	public void deleteTimeline(Timeline data);	//Deletes a specific timeline from the database.
 	public void deleteEvent(MyEvent data);	//Deletes a specific event from the database.
