8832a011f048e891d953e4a67b45dd8998ec335c
Hatem <hatem.houssein@gmail.com>
2015-04-23 10:58:24 +0200

    Commented out the database part of the class

--- Timeline/src/model/MyEvent.java
+++ Timeline/src/model/MyEvent.java
@@ -68,50 +68,50 @@ public abstract class MyEvent {
 		
 	}
 
-	public void updateToDatabase (ObjectContainer dataBase){
-		
-		boolean flag = false;
-		
-		ObjectSet retrieve = dataBase.query(MyEvent.class); // created object to retrieve info from the database
-
-		while (retrieve.hasNext()){// THERE IS AN ERROR IN THIS LOOP
-			MyEvent tmp=retrieve.next();
-			if (this.equals(tmp)){
-				tmp.setTitle(this.getTitle());
-				tmp.setDescription(this.getDescription());
-				flag=true;
-				this=tmp;
-				break;
-			}
-		}
-		
-		if (flag){
-			dataBase.store(this);
-			dataBase.commit();
-		}else{
-			System.out.println( "edit fail");
-		}
-		
-		
-	}
-	
-	public void updateToDatabase (ObjectContainer dataBase){
-		
-		ObjectSet retrieve = dataBase.query(MyEvent.class); // created object to retrieve info from the database
-		boolean flag = false;
-		while (retrieve.hasNext()){// THERE IS AN ERROR IN THIS LOOP
-			MyEvent tmp=retrieve.next();
-			if (this.equals(tmp)){
-				dataBase.delete(tmp);
-				dataBase.commit();
-				flag=true;
-				break;
-			}
-		}
-		if (!flag){
-			System.out.println( "delete fail!");
-		}
-	}
-}
+//	public void updateToDatabase (ObjectContainer dataBase){
+//		
+//		boolean flag = false;
+//		
+//		ObjectSet retrieve = dataBase.query(MyEvent.class); // created object to retrieve info from the database
+//
+//		while (retrieve.hasNext()){// THERE IS AN ERROR IN THIS LOOP
+//			MyEvent tmp=retrieve.next();
+//			if (this.equals(tmp)){
+//				tmp.setTitle(this.getTitle());
+//				tmp.setDescription(this.getDescription());
+//				flag=true;
+//				this=tmp;
+//				break;
+//			}
+//		}
+//		
+//		if (flag){
+//			dataBase.store(this);
+//			dataBase.commit();
+//		}else{
+//			System.out.println( "edit fail");
+//		}
+//		
+//		
+//	}
+//	
+//	public void updateToDatabase (ObjectContainer dataBase){
+//		
+//		ObjectSet retrieve = dataBase.query(MyEvent.class); // created object to retrieve info from the database
+//		boolean flag = false;
+//		while (retrieve.hasNext()){// THERE IS AN ERROR IN THIS LOOP
+//			MyEvent tmp=retrieve.next();
+//			if (this.equals(tmp)){
+//				dataBase.delete(tmp);
+//				dataBase.commit();
+//				flag=true;
+//				break;
+//			}
+//		}
+//		if (!flag){
+//			System.out.println( "delete fail!");
+//		}
+//	}
+//}
 
 }
