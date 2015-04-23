26e235799a952140f92e8ef7ebc4d6d88f07afb5
Hatem <hatem.houssein@gmail.com>
2015-04-23 10:44:31 +0200

    database testing

--- Timeline/src/model/databaseTest2.java
+++ Timeline/src/model/databaseTest2.java
@@ -1,31 +1,33 @@
 package model;
 
-import com.db4o.Db4o;
-import com.db4o.ObjectContainer;
-import com.db4o.ObjectSet;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+import com.db4o.*;
 
 public class databaseTest2 {
 
+	@SuppressWarnings("deprecation")
 	public static void main(String[] args) {
-		   ObjectContainer db = null; // creates the object data base
-	        
-	        try
-	        {
-	            db = Db4o.openFile("timelineDatabase.data"); // open file in the object
-				
-				ObjectSet retrieve = db.query(EventNT.class);
-				
-				while (retrieve.hasNext()){
-					System.out.println (retrieve.next().toString());
-				}
-	        }
-	        
-	        finally
-	        
-	        {
-	            if (db != null)
-	                db.close(); 
-	        }
-	    }
+		ObjectContainer  db = Db4o.openFile(Db4o.newConfiguration(), "timelineDatabase.data"); // open file in the object
+		try
+		{	
+			GregorianCalendar sol = new GregorianCalendar();
+			sol.setTime(new Date(1997, 10, 10));
+			GregorianCalendar sol1 = new GregorianCalendar();
+			sol1.setTime(new Date(1999, 10, 10));
+			
+			YearTimeline str = new YearTimeline("Hatem", "fed", sol, sol1);
+			db.store(str);
+			db.commit();
+			ObjectSet<YearTimeline> retrieve = db.queryByExample(str);
+			System.out.println (retrieve.toString());
+		}
 
+		finally
+		{
+			if (db != null)
+				db.close(); 
+		}
 	}
+}
