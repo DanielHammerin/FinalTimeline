65988a9d02468944c74c87e7b6eda8b561b3a756
Huan <timy0930@gmail.com>
2015-05-07 11:59:44 +0200

    event edit part 1

--- Timeline/src/model/MyEvent.java
+++ Timeline/src/model/MyEvent.java
@@ -1,7 +1,4 @@
 package model;
-
-import com.db4o.ObjectContainer;
-import com.db4o.ObjectSet;
 import java.util.Date;
 
 public abstract class MyEvent {
@@ -40,70 +37,4 @@ public abstract class MyEvent {
     	this.title = title;
 		this.description = description;
     }
-    
-public void addToDatabase (ObjectContainer dataBase){
-		
-		boolean flag = true;
-		
-		ObjectSet retrieve = dataBase.query(MyEvent.class); // created object to retrieve info from the database
-
-		while (retrieve.hasNext()){// THERE IS AN ERROR IN THIS LOOP
-			if (this.equals(retrieve.next())){
-				flag = false;
-			}
-		}
-		
-		if (flag){
-			dataBase.store(this);
-			dataBase.commit();
-		} else {
-			System.out.println( "The event has been already added in the timeline!");
-		}
-			
-	}
-	
-
-	public void updateToDatabase1 (ObjectContainer dataBase){
-		
-		boolean flag = false;
-		
-		ObjectSet retrieve = dataBase.query(MyEvent.class); // created object to retrieve info from the database
-
-		while (retrieve.hasNext()){// THERE IS AN ERROR IN THIS LOOP
-			MyEvent tmp=(MyEvent) retrieve.next();
-			if (this.equals(tmp)){
-				tmp.setTitle(this.getTitle());
-				tmp.setDescription(this.getDescription());
-				flag=true;
-				break;
-			}
-		}
-		
-		if (flag){
-			dataBase.store(this);
-			dataBase.commit();
-		}else{
-			System.out.println( "edit fail");
-		}	
-	}
-	
-	
-	public void updateToDatabase (ObjectContainer dataBase){
-		
-		ObjectSet retrieve = dataBase.query(MyEvent.class); // created object to retrieve info from the database
-		boolean flag = false;
-		while (retrieve.hasNext()){// THERE IS AN ERROR IN THIS LOOP
-			MyEvent tmp=(MyEvent) retrieve.next();
-			if (this.equals(tmp)){
-				dataBase.delete(tmp);
-				dataBase.commit();
-				flag=true;
-				break;
-			}
-		}
-		if (!flag){
-			System.out.println( "delete fail");
-		}
-	}
-
 }
